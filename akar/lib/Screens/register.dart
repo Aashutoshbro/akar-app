bee486656c893d09ef6085981515680d605e68fb
Hom bdr. pathak <pathakhom17@gmail.com>
2024-05-09 15:53:26 +0545

    added some registration and other files

---
+++ akar/lib/Screens/register.dart
@@ -0,0 +1,151 @@
+import "package:flutter/material.dart";
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  bool _obscureText = true;
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor:const Color(0xFF33396d),
+
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: Center(
+        child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 107.0),
+                    child: Row(children: [
+                      Text(
+                        'Register',
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepPurple),
+                        textAlign: TextAlign.center,
+                      )
+                    ]),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Image.asset(
+                    "assets/auth.png",
+                    height: 120,
+                    width: 120,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Full Name',
+                      prefixIcon: const Icon(Icons.person,
+                          color: Colors.black87, size: 30.0),
+
+                      // filled: true,
+                      //fillColor: const Color(0xFF9ee1d9),
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: const Icon(Icons.email),
+                      // filled: true,
+                      //fillColor: const Color(0xFF9ee1d9),
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    obscureText: _obscureText,
+
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: _togglePasswordVisibility,
+                      ),
+
+                      // filled: true,
+                      // fillColor: const Color(0xFF9ee1d9),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    //obscureText: true,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Contact Number',
+                      prefixIcon: const Icon(Icons.phone),
+                      // filled: true,
+                      //fillColor: const Color(0xFF9ee1d9),
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      //Sign in logic yaha
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple),
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  const Center(
+                    child: Text("Have an account?"),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Log In',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
